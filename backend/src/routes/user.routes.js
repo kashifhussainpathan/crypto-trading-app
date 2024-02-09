57aa035e8deb75f51c89c9159c32267eff661ec1
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-09 15:47:54 +0530

    remove unused functions

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -6,13 +6,10 @@ import {
   logoutUser,
   removeOrder,
   registerUser,
-  refreshAccessToken,
-  changeCurrentPassword,
+  updateOrder,
   getCurrentUser,
-  updateUserAvatar,
-  updateAccountDetails,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -24,15 +21,10 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
-router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router.route("/order").post(verifyJWT, addOrder);
 router.route("/order").get(verifyJWT, getOrders);
 router.route("/order/:orderId").delete(verifyJWT, removeOrder);
-
-router
-  .route("/avatar")
-  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/order/:orderId").patch(verifyJWT, updateOrder);
 
 export default router;
