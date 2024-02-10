a3b0ec798451519ad606f45074057608db5f41ec
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-10 18:03:53 +0530

    refacrtor: remove dependency

--- frontend/src/hooks/useGetCoinsPrice.tsx
+++ frontend/src/hooks/useGetCoinsPrice.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { setLivePrices } from "../features/assets/assetsSlice";
+import {
+  setHighlightedRows,
+  setLivePrices,
+} from "../features/assets/assetsSlice";
 
 const useGetCoinsPrice = () => {
   const dispatch = useDispatch();
-
-  const [highlightedRows, setHighlightedRows] = useState<string[]>([]);
-  const [coinPrices, setCoinPrices] = useState<Record<string, number>>({});
 
   useEffect(() => {
     const pricesWs = new WebSocket("wss://ws.coincap.io/prices?assets=ALL");
@@ -14,27 +14,23 @@ const useGetCoinsPrice = () => {
     pricesWs.onmessage = function (event) {
       const data = JSON.parse(event.data);
 
-      setCoinPrices((prevPrices) => ({ ...prevPrices, ...data }));
-
       dispatch(setLivePrices(data));
 
       const updatedHighlightedRows = [];
       for (const symbol in data) {
         updatedHighlightedRows.push(symbol);
       }
-      setHighlightedRows(updatedHighlightedRows);
+      dispatch(setHighlightedRows(updatedHighlightedRows));
 
       setTimeout(() => {
-        setHighlightedRows(() => []);
+        dispatch(setHighlightedRows([]));
       }, 1000);
     };
 
     return () => {
       pricesWs.close();
     };
-  }, [coinPrices]);
-
-  return { highlightedRows, coinPrices };
+  }, []);
 };
 
 export default useGetCoinsPrice;
