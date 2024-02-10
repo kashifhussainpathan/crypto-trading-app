cc3a699581dde546ba24ee45b9941c7218b0a4c3
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-10 17:10:12 +0530

    add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Trade from "./pages/trade";
 import Profile from "./pages/profile";
+import Navbar from "./components/navbar";
+import useGetCoinsPrice from "./hooks/useGetCoinsPrice";
 
 function App() {
+  useGetCoinsPrice();
+
   return (
     <main className="w-full text-gray-700">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Trade />} />
         <Route path="/profile" element={<Profile />} />
