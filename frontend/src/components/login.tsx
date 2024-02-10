919800a33b57ea711d7da07b4880cefb64d37a3a
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-10 17:10:44 +0530

    feat: add auth

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,21 +1,43 @@
-import { ChangeEvent, SyntheticEvent, useState } from "react";
-import { signInInputs } from "../constants/authConstants";
+import { ChangeEvent, FC, SyntheticEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import FormInput from "./formInput";
 import Button from "./@ui/button";
+import { signInInputs } from "../constants/authConstants";
+import { useLoginMutation } from "../features/user/userSlice";
 
-const Login = () => {
-  const [values, setValues] = useState<{ email: string; password: string }>({
+interface ILogin {
+  navigateAuth: () => void;
+}
+
+const Login: FC<ILogin> = ({ navigateAuth }) => {
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState<{
+    email: string;
+    password: string;
+    username: string;
+  }>({
     email: "",
     password: "",
+    username: "",
   });
 
-  const [isRegistering, setIsRegistering] = useState<boolean>(false);
+  const [login, { isLoading, error }] = useLoginMutation();
 
-  const logIn = async (email: string, password: string) => {};
-
-  const handleSubmit = (event: SyntheticEvent) => {
+  const handleSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
-    logIn(values.email, values.password);
+    try {
+      const { email, password, username } = values;
+
+      //@ts-ignore
+      const { data } = await login({ email, username, password });
+      localStorage.setItem("accessToken", data.data.accessToken);
+      localStorage.setItem("refreshToken", data.data.refreshToken);
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -23,28 +45,50 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div className="bg-gray-100 py-4 flex justify-between items-center px-4">
-        <p className="font-medium text-lg">Login</p>
-      </div>
-      <form className="flex flex-col items-center px-6 py-4">
-        {signInInputs?.map((input) => (
-          <FormInput
-            key={input.id}
-            {...input}
-            value={values[input.name as keyof typeof values] || ""}
-            onChange={onChange}
-          />
-        ))}
+    <>
+      <div className="w-[300px] mx-auto bg-white px-4 py-1 rounded-md bg-opacity-70 backdrop-filter backdrop-blur-3xl">
+        <div className="w-full bg-gray-100 py-2 flex justify-center items-center px-2 mt-4">
+          <p className="font-medium text-lg">Login</p>
+        </div>
+        <form className="flex flex-col items-center py-4 w-full">
+          {signInInputs?.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name as keyof typeof values] || ""}
+              onChange={onChange}
+            />
+          ))}
+          <Button onClick={handleSubmit} className="w-full">
+            {isLoading ? (
+              <div
+                className="animate-spin inline-block w-4 h-4 border-[3px] border-current border-t-transparent text-gray-400 rounded-full"
+                role="status"
+                aria-label="loading"
+              ></div>
+            ) : (
+              "Login"
+            )}
+          </Button>
 
-        <Button
-          onClick={handleSubmit}
-          classes="border-2 !border-gray-600 p-2 my-4 w-full"
+          {error && (
+            <p className="mt-4 text-red-500 text-center">
+              {/* @ts-ignore */}
+              {error?.data?.message}
+            </p>
+          )}
+        </form>
+      </div>
+      <p className="text-center text-white mt-1">
+        Don't have an account?{" "}
+        <span
+          className="text-blue-700  cursor-pointer"
+          onClick={() => navigateAuth()}
         >
-          {isRegistering ? "Loging in.." : "Login"}
-        </Button>
-      </form>
-    </div>
+          Signup
+        </span>{" "}
+      </p>
+    </>
   );
 };
 
--- frontend/src/components/signup.tsx
+++ frontend/src/components/signup.tsx
@@ -1,13 +1,14 @@
-import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { ChangeEvent, FC, SyntheticEvent, useState } from "react";
 import FormInput from "./formInput";
 import Button from "./@ui/button";
 import { signUnInputs } from "../constants/authConstants";
-import {
-  useLoginMutation,
-  useSignupMutation,
-} from "../features/user/userSlice";
+import { useSignupMutation } from "../features/user/userSlice";
 
-const SignUp = () => {
+interface navigateAuth {
+  navigateAuth: () => void;
+}
+
+const SignUp: FC<navigateAuth> = ({ navigateAuth }) => {
   const [values, setValues] = useState<{
     fullName: string;
     username: string;
@@ -22,26 +23,20 @@ const SignUp = () => {
     confirmPassword: "",
   });
 
-  // const [signup, { isLoading, error }] = useSignupMutation();
-  const [login, { isLoading, error }] = useLoginMutation();
+  const [signup, { isLoading, error }] = useSignupMutation();
 
   const handleSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
 
     try {
-      // await signup({
-      //   email: values.email,
-      //   password: values.password,
-      //   username: values.username,
-      //   fullName: values.fullName,
-      // });
-      const response = await login({
-        email: "kashifpathan@gmail.com",
-        username: "kashifpathan",
-        password: "kashif1@",
+      await signup({
+        email: values.email,
+        password: values.password,
+        username: values.username,
+        fullName: values.fullName,
       });
 
-      localStorage.setItem("accessToken", response?.data?.data.accessToken);
+      navigateAuth();
     } catch (error) {
       console.log("Signup failed:", error);
     }
@@ -62,32 +57,55 @@ const SignUp = () => {
   });
 
   return (
-    <div className="w-[300px] mx-auto bg-white px-4 py-1 rounded-md bg-opacity-70 backdrop-filter backdrop-blur-3xl">
-      <div className="w-full bg-gray-100 py-2 flex justify-center items-center px-2 mt-4">
-        <p className="font-medium text-lg">Create Account</p>
+    <>
+      <div className="w-[300px] mx-auto bg-white px-4 py-1 rounded-md bg-opacity-70 backdrop-filter backdrop-blur-3xl">
+        <div className="w-full bg-gray-100 py-2 flex justify-center items-center px-2 mt-4">
+          <p className="font-medium text-lg">Create Account</p>
+        </div>
+        <form className="flex flex-col items-center py-4 w-full">
+          {dynamicInputs?.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name as keyof typeof values] || ""}
+              onChange={onChange}
+            />
+          ))}
+
+          <Button
+            onClick={handleSubmit}
+            className="border-2 !border-gray-600 p-2 w-full !mt-1"
+          >
+            {isLoading ? (
+              <div
+                className="animate-spin inline-block w-4 h-4 border-[3px] border-current border-t-transparent text-gray-400 rounded-full"
+                role="status"
+                aria-label="loading"
+              ></div>
+            ) : (
+              "Create Account"
+            )}
+          </Button>
+
+          {error && (
+            <p className="mt-4 text-red-500 text-center">
+              {/* @ts-ignore */}
+              {error?.data?.message}
+            </p>
+          )}
+        </form>
       </div>
-      <form className="flex flex-col items-center py-4 w-full">
-        {dynamicInputs?.map((input) => (
-          <FormInput
-            key={input.id}
-            {...input}
-            value={values[input.name as keyof typeof values] || ""}
-            onChange={onChange}
-          />
-        ))}
 
-        <Button
-          onClick={handleSubmit}
-          className="border-2 !border-gray-600 p-2 w-full !mt-1"
+      <p className="text-center mt-1 text-white">
+        Already have an account ?{" "}
+        <span
+          className="text-blue-700 cursor-pointer"
+          onClick={() => navigateAuth()}
         >
-          {isLoading ? "Creating.." : "Create Account"}
-        </Button>
-
-        {error && (
-          <p className="mt-4 text-red-500 text-center">{error?.data.message}</p>
-        )}
-      </form>
-    </div>
+          Login
+        </span>{" "}
+      </p>
+    </>
   );
 };
 
