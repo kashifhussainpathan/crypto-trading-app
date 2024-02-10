edee651a591ed2034632ff1b09a8ce50f04622d1
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-10 18:05:31 +0530

    refactor

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import Login from "../components/login";
 import SignUp from "../components/signup";
+import UserCard from "../components/userCard";
+import { useGetUserQuery } from "../features/user/userSlice";
 
 const Profile = () => {
+  const token = localStorage.getItem("accessToken");
+
+  const { data: user } = useGetUserQuery(
+    { token },
+    { refetchOnMountOrArgChange: true }
+  );
+
+  const [toggleAuth, setToggleAuth] = useState<boolean>(false);
+
+  const handleToggleAuth = () => {
+    setToggleAuth(!toggleAuth);
+  };
+
   return (
-    <section className="h-screen bg-[url('./assets/crypto-bg.jpg')] bg-center bg-cover ">
-      <div className="h-screen w-screen bg-opacity-60 backdrop-filter backdrop-blur-sm grid place-content-center bg-gray-600">
-        <SignUp />
+    <section className="h-[90vh] bg-[url('./assets/crypto-bg.jpg')] bg-center bg-cover ">
+      <div className="h-[90vh] w-screen bg-opacity-60 backdrop-filter backdrop-blur-sm grid place-content-center bg-gray-600">
+        {user ? (
+          //@ts-ignore
+          <UserCard user={user} />
+        ) : toggleAuth ? (
+          <SignUp navigateAuth={handleToggleAuth} />
+        ) : (
+          <Login navigateAuth={handleToggleAuth} />
+        )}
       </div>
     </section>
   );
