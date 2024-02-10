c11650062a6896833cb4e60410dfb4f768400195
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-10 18:03:01 +0530

    feat: add api for auth

--- frontend/src/features/user/userSlice.ts
+++ frontend/src/features/user/userSlice.ts
@@ -1,9 +1,45 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IUser } from "../../models/user.type";
+import { IUserCard } from "../../components/userCard";
+import { IOrder } from "../../models/order.type";
+
+interface IRefreshTokenResponse {
+  refreshToken: string;
+  accessToken: string;
+}
+
+interface User {
+  _id: string;
+  username: string;
+  email: string;
+  fullName: string;
+  avatar: string;
+  balance: number;
+  orders: IOrder[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+interface LoginResponseData {
+  user: User;
+  accessToken: string;
+  refreshToken: string;
+}
+
+interface LoginResponse {
+  data: LoginResponseData;
+  statusCode: number;
+  message: string;
+  success: boolean;
+}
 
 export const userApi = createApi({
   reducerPath: "user",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/v1/users" }),
+
+  tagTypes: ["user"],
+
   endpoints: (builder) => ({
     signup: builder.mutation<IUser, {}>({
       query: (user) => ({
@@ -12,14 +48,50 @@ export const userApi = createApi({
         body: user,
       }),
     }),
-    login: builder.mutation<IUser, {}>({
+
+    login: builder.mutation<LoginResponse, IUser>({
       query: (user) => ({
         url: "/login",
         method: "POST",
         body: user,
       }),
+      invalidatesTags: ["user"],
+    }),
+
+    logout: builder.mutation<void, { token: string | null }>({
+      query: ({ token }) => ({
+        url: "/logout",
+        method: "POST",
+        headers: { Authorization: `Bearer ${token}` },
+      }),
+      invalidatesTags: ["user"],
+    }),
+
+    getUser: builder.query<IUserCard, { token: string | null }>({
+      query: ({ token }) => ({
+        url: "/current-user",
+        headers: { Authorization: `Bearer ${token}` },
+      }),
+      transformResponse: (rawData: { data: IUserCard }) => rawData.data,
+      providesTags: ["user"],
+    }),
+
+    refreshToken: builder.mutation<IRefreshTokenResponse, string | null>({
+      query: (refreshToken) => ({
+        url: "/refresh-token",
+        method: "POST",
+        body: refreshToken,
+      }),
+      transformResponse: (rawData: { data: IRefreshTokenResponse }) =>
+        rawData.data,
     }),
   }),
 });
 
-export const { useSignupMutation, useLoginMutation } = userApi;
+export const {
+  useGetUserQuery,
+  useLogoutMutation,
+  useSignupMutation,
+  useLoginMutation,
+  useRefreshTokenMutation,
+} = userApi;
